d2f7a822762b5a294ee16e0a188881b7993a5513
Зиновьев Кирилл <kirillzinovev23@gmail.com>
2023-11-30 15:18:18 +0300

    11

--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,6 @@ const User = require("../models/user");
 exports.form = (req, res) => {
   res.render(`register`, {});
 };
-exports.submit = (req, res) => {
+exports.submit = (req, res, next) => {
   User.create();
 };
